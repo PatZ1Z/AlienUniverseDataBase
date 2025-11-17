61925b999f2a81a061b153692303388196f7f53c
PatZ1Z <patryk.witkowski@cyfroweszkoly.pl>
2025-11-17 13:38:18 +0100

    Start of the project

---
+++ AlienUniverseDatabase/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AlienUniverseDatabase.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
