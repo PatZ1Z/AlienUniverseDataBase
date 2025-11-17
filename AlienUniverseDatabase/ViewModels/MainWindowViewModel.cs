160217ea6155161167a73830c5440d7db6001d23
PatZ1Z <patryk.witkowski@cyfroweszkoly.pl>
2025-11-17 14:16:51 +0100

    Added txt file reading

--- AlienUniverseDatabase/ViewModels/MainWindowViewModel.cs
+++ AlienUniverseDatabase/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,34 @@
 ﻿namespace AlienUniverseDatabase.ViewModels;
+using System;
+using System.IO;
+using ReactiveUI;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private string _filmyContent;
+
+    public string FilmyContent
+    {
+        get => _filmyContent;
+        set => this.RaiseAndSetIfChanged(ref _filmyContent, value);
+    }
+
+    private string FilmyPath = "/home/pat/RiderProjects/AlienUniverseDatabase/AlienUniverseDatabase/Assets/Data/filmyAlien.txt";
+
+    public MainWindowViewModel()
+    {
+        FilmyContent = ReadFile(FilmyPath);
+    }
+
+    private string ReadFile(string path)
+    {
+        try
+        {
+            return File.ReadAllText(path);
+        }
+        catch (Exception ex)
+        {
+            return $"Błąd: {ex.Message}";
+        }
+    }
 }
